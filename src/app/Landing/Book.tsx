382f9b1a1c9698fe93434a6d5ba1ae2b4af3c075
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-25 14:03:21 -0300

    corrigido posicionamento de textos.

--- src/app/Landing/Book.tsx
+++ src/app/Landing/Book.tsx
@@ -8,11 +8,11 @@ import Services from "@/app/components/Services";
 export default function Book() {
     return (
         <Area className="bg-white relative"> {/* Defina "relative" para criar um contexto de posicionamento */}
-            <div id="book" className="absolute inset-x-0 top-4 flex items-center justify-center text-6xl  md:text-7xl opacity-20 font-menu text-rose-gold bg-transparent  sm:top-36 lg:top-48">
+            <div id="book" className="absolute inset-x-0 top-4 flex items-center justify-center text-6xl  md:text-7xl opacity-20 font-menu text-rose-gold bg-transparent lg:text-9xl  sm:mt-5">
                 Agende Agora
             </div>
 
-            <div className="flex justify-center items-center py-10 sm:mt-32 lg:mt-48">
+            <div className="flex justify-center items-center py-10 sm:mt-20">
                 <Services />
             </div>
         </Area>
