94c3aa9f04ed0b6d97295e20eaf18ae7a22e0f82
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-25 13:08:20 -0300

    colocado imagens reais de trabalho.

--- src/app/Landing/Works.tsx
+++ src/app/Landing/Works.tsx
@@ -2,34 +2,45 @@
 import Image from "next/image";
 import Area from "@/app/components/Area";
 
+import work1 from "../../../public/works/work1.jpg";
+import work2 from "../../../public/works/work2.jpg";
+import work3 from "../../../public/works/work3.jpg";
+import work4 from "../../../public/works/work4.jpg";
+import work5 from "../../../public/works/work5.jpg";
+import work6 from "../../../public/works/work6.jpg";
+import work7 from "../../../public/works/work7.jpg";
+import work8 from "../../../public/works/work8.jpg";
+import work9 from "../../../public/works/work9.jpg";
+import work10 from "../../../public/works/work10.jpg";
+
 
 export default function Works() {
     return (
         <Area className="flex bg-container-gray border-zinc-300">
-            <div className="flex flex-col gap-4 md: my-5">
+            <div className="flex flex-col gap-2 md: my-5">
                 <div className="flex flex-col">
                     <span className="text-7xl opacity-10 text-rose-gold font-menu lg:text-9xl">Olhar Marcante</span>
                     <h1 className="text-white font-title text-4xl ">NOSSOS TRABALHOS</h1>
-                    <span className="text-text-normal text-xl text-justify my-4 items-center w-full md:w-1/2 ">
-                        Se você tem cílios curtos e raros, então a extensão de cílios é o que você precisa. Pode ver o resultado do nosso trabalho na galeria.
+                    <span className="text-text-normal text-xl text-justify my-4 items-center w-full ">
+                        Se você tem cílios curtos e raros, então a extensão de cílios é o que você precisa. Veja um pouco dos nossos trabalhos:
                     </span>
                 </div>
 
-                <div className="grid grid-cols-4 gap-4 lg:gap-x-28 lg:gap-y-9 z-10">
-                    <img className="rounded" src={"/works/work1.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work2.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work3.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work4.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work5.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work6.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work7.png"} alt={"foto de cílios antes e depois"} />
-                    <img className="rounded" src={"/works/work8.png"} alt={"foto de cílios antes e depois"} />
+                <div className="grid grid-cols-4 gap-4  z-10">
+                    <Image className="rounded" src={work1} alt={"foto de extensão de volume brasileiro linda feita pelo studio My Face Designer"} />
+                    <Image className="rounded" src={work2} alt={"extensão de cílios volume brasileiro feita pelo studio MyFace em curitiba"} />
+                    <Image className="rounded" src={work3} alt={"extensão de cílios volume russo feita pelo studio My Face em curitiba no bairro alto"} />
+                    <Image className="rounded" src={work4} alt={"extensão de cílios  volume russo pelo studio MyFace em curitiba"} />
+                    <Image className="rounded" src={work5} alt={"extensão de cílios volume brasileiro feita pelo studio MyFace em curitiba"} />
+                    <Image className="rounded" src={work6} alt={"foto de extensão de cílios linda feita pelo studio My Face Designer"} />
+                    <Image className="rounded" src={work7} alt={"extensão de cílios perfeita feita pelo studio MyFace em curitiba"} />
+                    <Image className="rounded" src={work8} alt={"extensão de cílios volume russo feita pelo studio My Face em curitiba no bairro alto"} />
 
                 </div>
 
                 <div className="flex justify-center mt-5 z-10 w-full lg:justify-between ">
-                    <Image className="rounded" src={"/works/work9.png"} alt={"foto de cílios antes e depois"} width={580} height={450} />
-                    <Image className="rounded hidden lg:inline" src={"/works/work10.png"} alt={"foto de cílios antes e depois"} width={580} height={450} />
+                    <Image className="rounded" src={work9} alt={"foto de cílios antes e depois"} width={580} height={450} />
+                    <Image className="rounded hidden lg:inline" src={work10} alt={"foto de cílios antes e depois"} width={580} height={450} />
                 </div>
 
 
