73ea14e05cb05967adff182f9bbff77d5b825b8f
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-26 16:48:01 -0300

    add container a rede do nginx

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+git pull
 docker stop myface-landing
 docker rm myface-landing
 docker build -t myface-landing .
-docker run -d -p 3005:3000 --name myface-landing myface-landing
+docker run -d -p 3005:3000 --name myface-landing --network nginx-proxy-manager_nginx-proxy myface-landing
