25a40a97194934afb49bd563fd1c21faade83ed2
Giorge Abdala <giorgeabdala@gmail.com>
2023-09-02 16:23:13 -0300

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,58 @@
 import './globals.css'
 import RootStyleRegistry from './emotion';
+import {Metadata} from "next";
 
   const title: string =  'MyFace Designer';
   const description: string = 'O amor em forma de extensão de cílios';
   const url: string = 'http://myfacedesigner.com.br';
+  const logo: string = 'http://myfacedesigner.com.br/myface.png';
 
+export const metadata: Metadata = {
+    metadataBase: new URL(url),
+    title: title,
+    description: description,
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-      <html lang="pt-BR">
-      <head >
-        <title>{title}</title>
-        <meta name="description" content={description} />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content="/logobackround.jpg" />
-          <meta property="og:url" content={url} />
+    openGraph: {
+        title: title,
+        description: description,
+        url: url,
+        siteName: 'MyFace Designer',
+        images: [
+            {
+                url: logo,
+                width: 800,
+                height: 600,
+            },
+            {
+                url: logo,
+                width: 1800,
+                height: 1200,
+            }
 
-      </head>
-      <body>
-      <RootStyleRegistry>{children}</RootStyleRegistry>
-      </body>
-      </html>
-  );
+        ],
+        locale: 'pt_BR',
+        type: 'website',
+
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: title,
+        description: description,
+        images: [logo],
+    } ,
 }
+
+
+export default function RootLayout({children,}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="pt-BR" className="dark">
+        <body>
+        {children}
+        </body>
+        </html>
+    );
+
+}
+
