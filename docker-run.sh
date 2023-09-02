a9b353c62f66f925f234b5620cfc2412457a358a
Giorge Abdala <giorgeabdala@gmail.com>
2023-09-02 16:24:12 -0300

    docker build antes de estopar container.

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull
+docker build -t myface-landing .
 docker stop myface-landing
 docker rm myface-landing
-docker build -t myface-landing .
 docker run -d -p 3005:3000 --name myface-landing --network nginx-proxy-manager_nginx-proxy myface-landing
